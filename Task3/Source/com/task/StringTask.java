7154d799b2a1195a2adefd4aa7c1d7936eafc51d
Prabhakaranselvan <prabhanarthamil@gmail.com>
2024-10-17 15:19:31 +0530

    3rd Feedback

--- Task3/Source/com/task/StringTask.java
+++ Task3/Source/com/task/StringTask.java
@@ -1,38 +1,21 @@
 package com.task;
+import com.helper.UtilsHub;
 import com.exception.InvalidException;
 
 public class StringTask 
 {
-//Method To Check Whether the String is Null or Empty
-	private void checkNull(Object str) throws InvalidException 
-	{
-        if (str == null ) 
-		{
-            throw new InvalidException("Given Input is Null");
-        }
-    }
 
-
-//Method To Check Whether The Given No is Within Range
-	private void checkWithinRange(int range, int length) throws InvalidException 	
-	{
-		
-        if (range > length || range < 0) 
-		{
-            throw new InvalidException("Input is not within Required Range");
-        }
-    }
 //Method for Exercise_1
    	public int getLength(String str) throws InvalidException
     {
-		checkNull(str);
+		UtilsHub.checkNull(str);
 		return str.length();
     }
 
 //Method for Exercise_2
     public char[] getCharArray(String str) throws InvalidException       
     {
-		checkNull(str);
+		UtilsHub.checkNull(str);
        	return str.toCharArray();
     }
 
@@ -40,22 +23,16 @@ public class StringTask
     public char getFromLast(String str, int fromLast) throws InvalidException		
     {
 		int length = getLength(str);
-		if (length == 0)
-		{
-			throw new InvalidException("Length cannot be zero");
-		}
-		if (fromLast == 0)
-		{
-			throw new InvalidException("Position cannot be zero");
-		}
-		checkWithinRange(fromLast, length);
+		UtilsHub.checkZero(length);
+		UtilsHub.checkZero(fromLast);
+		UtilsHub.checkWithinRange(fromLast, length);
 		return str.charAt(length - fromLast);
     }
 
 //Method for Exercise_4
 	public int getOccurance(String str, char letter) throws InvalidException		
     {
-		checkNull(str);
+		UtilsHub.checkNull(str);
 		int i = 0;
 		int occurance = 0;
 		int length = str.length();
@@ -71,18 +48,18 @@ public class StringTask
 	}
 
 //Method for Exercise_5
-	 public int getGreatestPosition(String str, char letter) throws InvalidException						
+	 public int getGreatestIndex(String str, char letter) throws InvalidException						
     {
-		checkNull(str);
+		UtilsHub.checkNull(str);
 		int i = str.lastIndexOf(letter);
-		return (i+1);
+		return (i);
     }
 		
 //Method for Exercise_6
 	public String getLastCharacters(String str, int last) throws InvalidException						
 	{
 		int length = getLength(str);
-       	checkWithinRange(last, length);
+       	UtilsHub.checkWithinRange(last, length);
 		return str.substring(length - last);
 	}
 
@@ -90,7 +67,7 @@ public class StringTask
 	public String getFirstCharacters(String str, int first) throws InvalidException						
 	{
 		int length = getLength(str);
-       	checkWithinRange(first, length);
+       	UtilsHub.checkWithinRange(first, length);
 		return str.substring(0,first);
 	}
 
@@ -99,8 +76,8 @@ public class StringTask
 	{
 		int length = getLength(str);
        	int replaceCharsLen = getLength(replaceChars);
-       	checkWithinRange(replaceCharsLen, length);
-		return str.replaceFirst(str.substring(0,noOfChar), replaceChars);
+		UtilsHub.checkWithinRange(noOfChar, length);
+		return str.replaceFirst(str.substring(0,noOfChar), replaceChars);	
 	}
 
 //Method for Exercise_8
@@ -108,37 +85,37 @@ public class StringTask
 	{
 		int length = getLength(str);
        	int replaceCharsLen = getLength(replaceChars);
-       	checkWithinRange(replaceCharsLen, length);
+       	UtilsHub.checkWithinRange(noOfChar, length);
 		return str.substring(0, length - noOfChar) + replaceChars;
 	}
 
 //Method for Exercise_9
 	public boolean checkStartsWith(String str, String startsWith) throws InvalidException				
 	{
-		checkNull(str);
-       	checkNull(startsWith);
+		UtilsHub.checkNull(str);
+       	UtilsHub.checkNull(startsWith);
 		return str.startsWith(startsWith);
 	}
 
 //Method for Exercise_10
 	public boolean checkEndsWith(String str, String endsWith)	throws InvalidException	
 	{
-		checkNull(str);
-       	checkNull(endsWith);
+		UtilsHub.checkNull(str);
+       	UtilsHub.checkNull(endsWith);
 		return str.endsWith(endsWith);
 	}
 
 //Method for Exercise_11	
 	public String changeToLowerCase(String str) throws InvalidException
 	{
-		checkNull(str);
+		UtilsHub.checkNull(str);
 		return str.toLowerCase();
 	}
 
 //Method for Exercise_12
 	public String changeToUpperCase(String str)	 throws InvalidException
 	{
-		checkNull(str);
+		UtilsHub.checkNull(str);
 		return str.toUpperCase();
 	}
 
@@ -163,51 +140,51 @@ public class StringTask
 //Method for Exercise_14
 	public String getMultipleStringLine(String str) throws InvalidException
 	{
-		checkNull(str);
+		UtilsHub.checkNull(str);
 		return str;
 	}
 
 //Method for Exercise_15
 	public String getConcatenatedLine(String str, String concatenateAt, String concatenateWith) throws InvalidException
 	{
-		checkNull(str);
+		UtilsHub.checkNull(str);
 		return str.replaceAll(concatenateAt, concatenateWith);
 	}
 
 //Method for Exercise_16
 	public String[] getStringArray(String str, String splitAt) throws InvalidException
 	{
-		checkNull(str);
+		UtilsHub.checkNull(str);
 		return str.split(splitAt);
 	}
 
 //Method for Exercise_17
 	public String getMergedStrings(String toJoin,String[] mergedStringArray) throws InvalidException
 	{
-       	checkNull(mergedStringArray);
+       	UtilsHub.checkNull(mergedStringArray);
 		return String.join(toJoin,mergedStringArray);
 	}
 
 //Method for Exercise_18
 	public boolean checkEquals(String str1, String str2) throws InvalidException
 	{
-		checkNull(str1);
-        checkNull(str2);
+		UtilsHub.checkNull(str1);
+        UtilsHub.checkNull(str2);
 		return str1.equals(str2);
 	}
 
 //Method for Exercise_19
 	public boolean checkEqualsIgnoreCase(String str1,String str2) throws InvalidException
 	{
-		checkNull(str1);
-        checkNull(str2);
+		UtilsHub.checkNull(str1);
+        UtilsHub.checkNull(str2);
 		return str1.equalsIgnoreCase(str2);
 	}
 
 //Method for Exercise_20
 	public String getTrim(String str) throws InvalidException
 	{
-		checkNull(str);
+		UtilsHub.checkNull(str);
 		return str.trim();
 	}
 
